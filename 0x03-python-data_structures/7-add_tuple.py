019d0284a86e87504317b87fd0c30ff7881bbfdf
ndundastevn <ndundastevn@gmail.com>
2023-05-15 06:17:30 -0700

    alx project

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    len1 = len(tuple_a)
-    len2 = len(tuple_b)
-
-    #check len for tuple_a
-    if len1 == 0:
-        tuple_a = (0,0)
-
-    elif len1 < 2:
+    lenA = len(tuple_a)
+    lenB = len(tuple_b)
+# check for tuple_a
+    if lenA < 1:
+        tuple_a = (0, 0)
+    elif lenA < 2:
         tuple_a = (tuple_a[0], 0)
 
-    # check len for tuple_b
-    if len2 == 0:
+# check for tuple_b
+    if lenB < 1:
         tuple_b = (0, 0)
-
-    elif len2 < 2:
+    elif lenB < 2:
         tuple_b = (tuple_b[0], 0)
 
-    sum_tup = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    return sum_tup
+    res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return res
