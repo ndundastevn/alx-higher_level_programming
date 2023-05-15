0228e81eccb5e9586d8617be42f1803b5d5d326f
ndundastevn <ndundastevn@gmail.com>
2023-05-15 02:43:12 -0700

    alx project

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
 
-    return ("".join(listofchars))
+def no_c(my_string):
+    new_s = my_string.translate({ord('c'): None})
+    new_s = new_s.translate({ord('C'): None})
+    return new_s
