9169ef0d3483fbad2dad8fed0e0c8deac60a2ecb
ndundastevn <ndundastevn@gmail.com>
2023-05-17 04:07:57 -0700

    alx project

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_copy = matrix.copy()
-    new_matrix = []
-    for i in matrix_copy:
-        new_max = [
-            i[0]*i[0],
-            i[1]*i[1],
-            i[2]*i[2]
-        ]
-        new_matrix.append(new_max)
-
-    return new_matrix
+    return [list(map((lambda x: x * x), elm)) for elm in matrix]
