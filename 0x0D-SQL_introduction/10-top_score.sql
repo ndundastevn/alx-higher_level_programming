0b133f87ecf0c7749599891a49d95abf0bdf24a9
ndundastevn <ndundastevn@gmail.com>
2023-07-19 02:39:58 -0700

    alx project

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records and display name and score from  second_table.
+-- Records are ordered by score in descending.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
