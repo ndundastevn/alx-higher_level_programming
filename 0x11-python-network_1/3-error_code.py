f62b85e8c19df8a36d7e36202ab80c51d1e0b83b
ndundastevn <ndundastevn@gmail.com>
2023-09-02 04:16:37 -0700

    alx project

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+import urllib.request
+import sys
+
+url = sys.argv[1]
+req = urllib.request.Request(url)
+try:
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("ascii"))
+except urllib.error.HTTPError as e:
+    print("Error code: {}".format(e.code))
