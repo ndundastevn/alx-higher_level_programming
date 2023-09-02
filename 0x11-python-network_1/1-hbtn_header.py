ab5a807666edd554c0efd69d8b3e6c5a26434f78
ndundastevn <ndundastevn@gmail.com>
2023-09-02 03:06:17 -0700

    alx project

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,7 +3,6 @@
 a Python script that takes in a URL, sends a request to the URL and dis"""
 import sys
 import urllib.request
-
 if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
