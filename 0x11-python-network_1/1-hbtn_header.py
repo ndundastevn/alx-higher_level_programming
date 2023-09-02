3d3c7949f51d5973e64076eefd9c3443890a7829
ndundastevn <ndundastevn@gmail.com>
2023-09-02 03:08:58 -0700

    alx project

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-a Python script that takes in a URL, sends a request to the URL and dis"""
+a Python script that takes in a URL, sends a request to the URL and display the value of X-Request-Id"""
 import sys
 import urllib.request
+
 if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
