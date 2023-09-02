abcb2a0158b04a5f36a80b5ea2c64320e0054f56
ndundastevn <ndundastevn@gmail.com>
2023-09-02 04:18:30 -0700

    alx project

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -7,6 +7,6 @@ url = sys.argv[1]
 req = urllib.request.Request(url)
 try:
     with urllib.request.urlopen(req) as response:
-        print(response.read().decode("ascii"))
+        print(response.read().decode("utf-8"))
 except urllib.error.HTTPError as e:
     print("Error code: {}".format(e.code))
