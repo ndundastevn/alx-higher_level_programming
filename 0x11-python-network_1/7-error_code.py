ec951ea8bcef012fb8ebd23d0485423dc3256f24
ndundastevn <ndundastevn@gmail.com>
2023-09-05 05:49:50 -0700

    alx project

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""sends a request to URL passed as urgument and displays the 
-body of the response"""
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 import sys
 import requests
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = requests.get(url)
-    if req.status_code >= 400:
-        print("Error code: {}".format(req.status_code))
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(req.text)
+        print(r.text)
